c59413a4b5ee43e3ef2f06190a64f6f08c825c22
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2025-08-18 12:42:10 +0800

    Create 3rdquestions.py

---
+++ review_questions/3rdquestions.py
@@ -0,0 +1,13 @@
+"""number 1 """
+name = "Whats your name? "
+print("Hi! " + input(name))
+print(" ")
+"""number 2 """
+Sname = "What is your Surname? "
+newword = input(Sname)
+print(newword.lower())
+print(" ")
+"""number 3 """
+string1 = "Write Anything: "
+newstring = input(string1)
+print(len(newstring))
