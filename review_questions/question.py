31580629220fecd6a7ef0ba808315e8027740f56
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2025-08-18 12:14:35 +0800

    Create question.py

---
+++ review_questions/question.py
@@ -0,0 +1,10 @@
+name = "Lourdyn"
+print(len(name))
+"""number 2 """
+name2 = "Verdida"
+print(name + name2)
+"""number 3 """
+print(name + " " + name2)
+"""number 4 """
+text = "The quick brown fox jumped over the lazy dog. #1234567890!"
+print(text[16:19])
