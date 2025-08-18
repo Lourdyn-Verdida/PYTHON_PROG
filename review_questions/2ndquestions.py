fdf5595bf9af7f4b7258f2c8f5115206690ba8c8
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2025-08-18 12:26:25 +0800

    Create 2ndquestions.py

---
+++ review_questions/2ndquestions.py
@@ -0,0 +1,31 @@
+"""number 1 """
+txt1 = "Animals"
+txt2 = "Badger"
+txt3 = "Honey Bee"
+txt4 = "Honeybadger"
+print(txt1.lower())
+print(txt2.lower())
+print(txt3.lower())
+print(txt4.lower())
+print("  ")
+"""number 2 """
+print(txt1.upper())
+print(txt2.upper())
+print(txt3.upper())
+print(txt4.upper())
+print("  ")
+"""number 3 """
+string1 =" filletmignon"
+string2 ="Brisket "
+print(string1.lstrip())
+print(string2.rstrip())
+print("  ")
+"""number 4 """
+string3="Becomes"
+string4="becomes"
+print(string3.startswith('be'))
+print(string4.startswith('be'))
+print("  ")
+"""number 5"""
+print(string3.startswith('Be'))
+print(string4.startswith('Be'))
